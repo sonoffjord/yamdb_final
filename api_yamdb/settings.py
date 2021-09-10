7748acda7a70bad592d801c45c04160a9a4abe36
Aleksandr Repin <sonoffjord@gmail.com>
2021-09-10 18:37:44 +0300

    update workflow

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', default='localhost').split(', ')
 
